a861157ec31f5534956e5f91e0ce9589802423e4
Valentina Apanina <bahtina-96@mail.ru>
2024-05-08 19:35:05 +0700

    Close

--- Less3/handlers/career_choice.py
+++ Less3/handlers/career_choice.py
@@ -1,6 +1,6 @@
 from aiogram import Router, types, F
 from aiogram.filters.command import Command
-from aiogram.fsm.context import  FSMContext
+from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from keyboards.career_keyboard import make_keyboard
 
@@ -9,15 +9,15 @@ router = Router()
 
 
 available_jobs = [
-    'Программист'
-    'Маркетолог'
-    'Менеджер'
-    'Аналитик'
+    'Программист',
+    'Маркетолог',
+    'Менеджер',
+    'Аналитик',
     'Бухгалтер'
 ]
 available_grades = [
-    'Низкий'
-    'Средний'
+    'Низкий',
+    'Средний',
     'Высокий'
 ]
 
@@ -29,27 +29,28 @@ class Choice(StatesGroup):
 
 @router.message(Command(commands=['prof']))
 async def start(message: types.Message, state: FSMContext):
-    await message.answer(text='Какая профессия Вас интересует?', reply_markup=make_keyboard(available_jobs))
+    await message.answer('Какая профессия вас интересует?', reply_markup=make_keyboard(available_jobs))
     await state.set_state(Choice.job)
 
 
 @router.message(Choice.job, F.text.in_(available_jobs))
 async def jobs(message: types.Message, state: FSMContext):
-    await message.answer(text='Как Вы оцениваете свою профессию', reply_markup=make_keyboard(available_grades))
+    await message.answer('Как вы оцениваете свою профессию?', reply_markup=make_keyboard(available_grades))
     await state.set_state(Choice.grade)
 
 
 @router.message(Choice.job)
-async def job_incorrect(message: types.Message):
-    await message.answer(text='Неправильно. Попробуйте еще раз', reply_markup=make_keyboard(available_jobs))
+async def job_incorrectly(message: types.Message):
+    await message.answer('Неправильно. Попробуйте ещё раз', reply_markup=make_keyboard(available_jobs))
 
 
 @router.message(Choice.grade, F.text.in_(available_grades))
 async def grade(message: types.Message, state: FSMContext):
-    await message.answer(text=f'Вы все прошли. С Вами свяжутся наши hr {message.text}',
-                        reply_markup=types.ReplyKeyboardRemove())
+    await message.answer(f'Вы всё прошли, с вами свяжутся наши hr {message.text}',
+                         reply_markup=types.ReplyKeyboardRemove())
     await state.clear()
 
+
 @router.message(Choice.grade)
-async def grade_incorrect(message: types.Message):
-    await message.answer(text='Неправильно. Попробуйте еще раз', reply_markup=make_keyboard(available_grades))
+async def grade_incorrectly(message: types.Message):
+    await message.answer('Неправильно. Попробуйте ещё раз', reply_markup=make_keyboard(available_grades))
--- Less3/main.py
+++ Less3/main.py
@@ -5,19 +5,19 @@ import logging
 from handlers import common, msg, career_choice
 
 
-
-
 async def main():
+    # Логирование
     logging.basicConfig(level=logging.INFO)
 
+    # Объект бота и диспетчера
     bot = Bot(token=config.token)
     dp = Dispatcher()
 
     dp.include_router(common.router)
-    dp.include_router(msg.career_choice_router)
+    dp.include_router(career_choice.router)
     dp.include_router(msg.router)
 
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     asyncio.run(main())
